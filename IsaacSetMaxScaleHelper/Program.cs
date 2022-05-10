6db00a53568e51eaffdd8b4db3568f41c34c0e23
laceous <laceous@gmail.com>
2022-05-10 14:32:09 -0700

    add to github

---
+++ IsaacSetMaxScaleHelper/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsaacSetMaxScaleHelper
+{
+    internal class Program
+    {
+        // server address + port
+        private const string LOCALHOST = "127.0.0.1";
+        private const int PORT = 11568;
+
+        // current as of Repentance (found with Spy++)
+        private const string ISAAC_CLASSNAME = "GLFW30";
+        private const string ISAAC_WINDOWNAME = "Binding of Isaac: Repentance";
+
+        // https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-findwindowa
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        // https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getwindowrect
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+        // used by GetWindowRect
+        [StructLayout(LayoutKind.Sequential)]
+        private struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+
+        // https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-setwindowpos
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        // flags defined for SetWindowPos
+        private const uint SWP_NOMOVE = 0x0002;
+        private const uint SWP_NOACTIVATE = 0x0010;
+        private const uint SWP_NOZORDER = 0x0004;
+
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Attempting to start server on {0}:{1}", LOCALHOST, PORT);
+
+                TcpListener server = new TcpListener(IPAddress.Parse(LOCALHOST), PORT);
+                server.Start();
+                
+                Console.WriteLine("Server started on {0}:{1}", LOCALHOST, PORT);
+
+                while (true)
+                {
+                    byte[] buffer = new byte[50];
+
+                    using (TcpClient client = await server.AcceptTcpClientAsync())
+                    {
+                        Console.WriteLine("Accepted connection");
+
+                        using (NetworkStream stream = client.GetStream())
+                        {
+                            await stream.ReadAsync(buffer);
+                        }
+                    }
+
+                    string message = Encoding.UTF8.GetString(buffer, 0, buffer.Length).TrimEnd('\0');
+                    Console.WriteLine("Message: " + message);
+
+                    if (message == "IsaacSetMaxScaleHelper")
+                    {
+                        ToggleWindowSize();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Server error");
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private static void ToggleWindowSize()
+        {
+            try
+            {
+                IntPtr hWnd = FindWindow(ISAAC_CLASSNAME, ISAAC_WINDOWNAME);
+
+                if (hWnd != IntPtr.Zero)
+                {
+                    Console.WriteLine("Found Isaac window: {0}, {1}", ISAAC_CLASSNAME, ISAAC_WINDOWNAME);
+
+                    if (GetWindowRect(hWnd, out RECT lpRect))
+                    {
+                        int width = lpRect.Right - lpRect.Left;
+                        int height = lpRect.Bottom - lpRect.Top;
+
+                        if (SetWindowPos(hWnd, IntPtr.Zero, 0, 0, width - 1, height, SWP_NOMOVE | SWP_NOACTIVATE | SWP_NOZORDER) &&
+                            SetWindowPos(hWnd, IntPtr.Zero, 0, 0, width, height, SWP_NOMOVE | SWP_NOACTIVATE | SWP_NOZORDER))
+                        {
+                            Console.WriteLine("Toggled window size");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Unable to toggle window size");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Unable to toggle window size");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Unable to find Isaac window: {0}, {1}", ISAAC_CLASSNAME, ISAAC_WINDOWNAME);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Windows API error");
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # IsaacSetMaxScaleHelper
+
+This was part of a POC for the [Set Max Scale](https://github.com/laceous/isaac-mod-set-max-scale) mod. Setting MaxScale while Isaac is running doesn't do anything by itself. You need to trigger an update by toggling fullscreen or resizing the window. You can toggle fullscreen from Lua code. This was an attempt to resize the window via the [SetWindowPos](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-setwindowpos) function.
+
+Ultimately, I wasn't very happy with this. I don't think it behaves any better than toggling fullscreen, and it opens up a security risk.
+
+## Instructions
+
+1. Run the IsaacSetMaxScaleHelper program
+2. Run Isaac with `--luadebug`
+3. Use the following Lua code instead of toggling fullscreen
+
+```lua
+local socketLoaded, socket = pcall(require, 'socket')
+
+if socketLoaded then
+  local tcp = socket.tcp()
+  tcp:connect('localhost', 11568)
+  tcp:send('IsaacSetMaxScaleHelper')
+  tcp:close()
+end
+```
